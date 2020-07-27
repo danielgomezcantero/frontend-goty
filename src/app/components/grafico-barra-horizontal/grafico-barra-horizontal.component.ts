c4feeaf51b6939ca8daf4c056e4d71dae8e47ac3
danielgomezcantero <danielgomezcantero@gmail.com>
2020-07-27 06:57:35 -0400

    Gráficos en tiempo real -NgxChartsModule

---
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent implements OnInit, OnDestroy {
+
+  
+  ngOnInit(): void {
+  }
+  results: any[]=[
+    {
+      "name": "Juego 1",
+      "value": 17
+    },
+    {
+      "name": "Juego 2",
+      "value": 25
+    },
+    {
+      "name": "Juego 3",
+      "value": 50
+    },
+    {
+      "name": "Juego 4",
+      "value": 30
+    }
+  ];
+ 
+
+ 
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
+
+  intervalo;
+
+  constructor() {
+
+    this.intervalo = setInterval(()=>{
+
+      console.log('tick');
+      
+       const newResults = [...this.results];
+
+      for( let i in newResults){
+        newResults[i].value= Math.round( Math.random() * 500 ) ;
+      }
+
+      this.results = [...newResults]
+
+    }, 1500 );
+
+  }
+
+
+  onSelect(event) {
+    console.log(event);
+  }
+  ngOnDestroy(){
+    clearInterval(this.intervalo);
+  }
+
+}
